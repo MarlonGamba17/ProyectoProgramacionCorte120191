3311d917f187088f57a1b00213e54867d0a0e999
darcy2411 <dnovoad87362@universidadean.edu.co>
2019-02-24 17:15:27 -0500

    Documentaciòn funcion calcular servicio extras

--- funciones.py
+++ funciones.py
@@ -17,7 +17,15 @@ def calcular_precio_servicio(cantidad_horas):
 
 def calcular_precio_servicio_extras(cantidad_horas):
     '''
-
+    (float) -> float
+    
+    Calcular el precio de  las horas extras prestadas en un servicio dada la cantidad de horas extras
+    TASA EXTRA= float contiene el valor de la tasa extra asignado al 25% 
+    tarifa_hora= float representa el valor de tarifa_hora asignado en 100000
+    precio_servicio_sin_recargo_extras= float contiene el precio de servicio sin el recargo de horas extras
+    precio_servicio_extras= float contiene el precio de servicio con el recago de las horas extras
+    
+    return= float retorna al valor de servicio con extras  
     '''
 
 
@@ -27,6 +35,7 @@ def calcular_costo_envio(kilometros):
 def calcular_precio_producto_fuera(coste_producto,
                                    kilometros):
     '''
+
 
     '''
 
