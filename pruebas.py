2c06f9aacd45788d58a2850a59fc840d6273bcfc
MarlonGamba17 <mgambav18183@universidadean.edu.co>
2019-02-24 22:06:44 -0500

    Actualizacion_PruebasM

--- pruebas.py
+++ pruebas.py
@@ -4,26 +4,29 @@ import funciones as f
 class pruebas(unittest.TestCase):
 
     def test_calcular_precio_producto(self):
-        self.assertEqual(f.calcular_precio_producto(1000), 1500)
-        self.assertEqual(f.calcular_precio_producto(0), 0)
+        self.assertEqual(1600, f.calcular_precio_producto(1000))
+        self.assertEqual(0, f.calcular_precio_producto(0))
 
     def test_calcular_precio_servicio(self):
         pass
 
     def test_calcular_precio_servicio_extras(self):
-        pass
+        self.assertEqual(750000.0, f.calcular_precio_servicio_extras(6))
+        self.assertEqual(1250000.0, f.calcular_precio_servicio_extras(10))
 
     def test_calcular_costo_envio(self):
         pass
 
     def test_calcular_precio_producto_fuera(self):
-        pass
+        self.assertEqual(1960, f.calcular_precio_producto_fuera(1000, 4))
+        self.assertEqual(3690, f.calcular_precio_producto_fuera(2000, 6))
 
     def test_calcular_iva_producto(self):
         pass
 
     def test_calcular_iva_servicio(self):
-        pass
+        self.assertEqual(57000, f.calcular_iva_servicio(3, 0.19))
+        self.assertEqual(32000, f.calcular_iva_servicio(2, 0.16))
 
     def test_calcular_iva_envio(self):
         pass
