e106cda7d6af234d89f20656da000c22da3893b9
pcaball71927 <pcaball71927@universidadean.edu.co>
2019-02-25 23:46:35 -0500

    Update pruebas.py
    correcciòn resultado pruebas

--- pruebas.py
+++ pruebas.py
@@ -6,10 +6,12 @@ class pruebas(unittest.TestCase):
     def test_calcular_precio_producto(self):
         self.assertEqual(f.calcular_precio_producto(1000),1500.0)
         self.assertEqual(f.calcular_precio_producto(0),0)
+        self.assertEqual(f.calcular_precio_producto(2500),3750.0)
 
     def test_calcular_precio_servicio(self):
         self.assertEqual(f.calcular_precio_servicio(48),4800000.0)
         self.assertEqual(f.calcular_precio_servicio(0),0.0)
+        self.assertEqual(f.calcular_precio_servicio(100000000),10000000000000.0)
 
     def test_calcular_precio_servicio_extras(self):
         self.assertEqual(f.calcular_precio_servicio_extras(6), 750000.0)
@@ -36,7 +38,7 @@ class pruebas(unittest.TestCase):
         self.assertEqual(f.calcular_iva_envio(30, 0.12), 414.0)
 
     def test_calcular_iva_servicio_extra(self):
-        self.assertEqual(f.calcular_iva_servicio_extra(15, 0.8),12.0)
+        self.assertEqual(f.calcular_iva_servicio_extra(15, 0.8),1500000.0)
         self.assertEqual(f.calcular_iva_servicio_extra(0, 0.8),0)
 
     def test_calcular_recaudo_locales(self):
@@ -45,7 +47,7 @@ class pruebas(unittest.TestCase):
         self.assertEqual(f.calcular_recaudo_locales(800,  700 ,   1 ), 2251.5)     
 
     def test_calcular_recaudo_horas_extra(self):
-        self.assertEqual(f.calcular_recaudo_horas_extra(2.5, 5, 6, 18), 31.5)
+        self.assertEqual(f.calcular_recaudo_horas_extra(2.5, 5, 6, 18), 3937500.0)
         self.assertEqual(f.calcular_recaudo_horas_extra(0, 0, 0, 0), 0)
 
     def test_calcular_recaudo_mixto_local(self):
